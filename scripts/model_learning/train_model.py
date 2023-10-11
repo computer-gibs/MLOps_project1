51848316ecab84e23643d030918987bfc66d08fe
computer-gibs <computer_gibs@mail.ru>
2023-10-11 21:58:41 +0500

    added script for training and saving the model

---
+++ scripts/model_learning/train_model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+import pickle
+
+train = pd.read_csv('../../data/processed/train_final.csv')
+
+preprocessor = ColumnTransformer(transformers=[
+    ('cat', OneHotEncoder(), ['CountryCode', 'Status', 'ReactorType', 'ReactorModel'])
+])
+
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('model', model)
+])
+
+X = train.drop('Capacity', axis=1)
+y = train['Capacity']
+
+pipeline.fit(X, y)
+
+with open('../../models/model.pkl', 'wb') as f:
+    pickle.dump(pipeline, f)
